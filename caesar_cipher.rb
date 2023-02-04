763c7b05a9e29072befdd033673f0c55f843f436
ngbinh29 <ng.d.binh.29@gmail.com>
2023-02-04 22:02:12 +0700

    update: determine case using ranges instead of regex

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,17 +2,20 @@
 # then use factor to transform into new character from newly added code
 # combine each char into new text
 # ! Note that character must be in alphabet and don't cipher anything that is not alphabetical
+LOWER_A = 97
+UPPER_A = 65
 def caesar_cipher(plaintext, factor)
   cipher_text = ''
+  upper_range = (UPPER_A...(UPPER_A + 26))
+  lower_range = (LOWER_A...(LOWER_A + 26))
   plaintext.each_char do |char|
-    if char.match?(/[a-z]/)
-      LOWER_A = 97
-      cipher_index = (char.ord - LOWER_A + factor) % 26
+    char_decimal = char.ord
+    if lower_range.include?(char_decimal)
+      cipher_index = (char_decimal - LOWER_A + factor) % 26
       cipher_char = (cipher_index + LOWER_A).chr
       cipher_text += cipher_char
-    elsif char.match?(/[A-Z]/)
-      UPPER_A = 65
-      cipher_index = (char.ord - UPPER_A + factor) % 26
+    elsif upper_range.include?(char_decimal)
+      cipher_index = (char_decimal - UPPER_A + factor) % 26
       cipher_char = (cipher_index + UPPER_A).chr
       cipher_text += cipher_char
     else
